4a8e6390231c988545ab921ee3d2e1a0766c73e5
Adam Cook <Adam.Cook@bbc.co.uk>
2017-02-28 14:06:35 +0000

    Initial commit.

---
+++ main.rs
@@ -0,0 +1,24 @@
+use std::env;
+
+
+
+fn main () {
+    let args: Vec<_> = env::args().collect();
+    let tokens: Vec<&str> = args[1].split(" ").collect();
+    let mut output: Vec<i32> = vec![];
+    let mut operator: Vec<_> = vec![];
+
+    for i in 1..tokens.len() {
+        let r = tokens[i].parse::<i32>();
+
+        match r {
+            Ok(_) => output.push(r.unwrap()),
+            Err(_) => {
+                if operator.len() > 0 {
+
+                }
+                operator.push(&tokens[i])
+            }
+        }
+    }
+}

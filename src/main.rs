15ee7c49d2d99ceb5c6e3dc3f491a6a654945fa8
Adam Cook <Adam.Cook@bbc.co.uk>
2017-03-06 16:44:53 +0000

    Different approach...

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use std::env;
 use std::collections::HashMap;
-
+#[allow(dead_code)]
 struct Operator {
     precedence: i32,
     func: Box<Fn(f32, f32) -> f32>
 }
 
-fn main () {
-    let args: Vec<_> = env::args().collect();
-    let tokens: Vec<&str> = args[1].split(" ").collect();
-    let mut output: Vec<i32> = vec![];
-    let mut operator: Vec<_> = vec![];
-
+fn get_operators () -> HashMap<char, Operator> {
     let multiply = Operator {
         precedence: 3,
         func: Box::new(move |a: f32, b:f32| a * b)
@@ -32,23 +27,41 @@ fn main () {
         func: Box::new(move |a: f32, b:f32| a - b)
     };
 
-    let mut operators: HashMap<char, Operator> = HashMap::new();
+    let mut operators = HashMap::new();
 
     operators.insert('*', multiply);
     operators.insert('/', divide);
     operators.insert('+', add);
     operators.insert('-', subtract);
 
-    for i in 1..tokens.len() {
-        let r = tokens[i].parse::<i32>();
+    operators
+}
 
-        match r {
-            Ok(_) => output.push(r.unwrap()),
-            Err(_) => {
-                if operator.len() > 0 {
+fn main () {
+    let args: Vec<_> = env::args().collect();
+    let tokens: Vec<&str> = args[1].split(" ").collect();
+    let mut operator_stack: Vec<char> = vec![];
+    let mut output: Vec<String> = vec![];
+    let operators = get_operators();
+
+    for t in tokens {
+        let maybeInt = t.parse::<i32>();
+
+        match maybeInt {
+            Ok(a) => output.push(t.to_string()),
+            Err(a) => {
+                if operator_stack.len() > 0 {
+                    let mut operator_char = t.to_string().pop().unwrap();
+                    let o1 = operators.get(&operator_char).unwrap();
+                    operator_char = operator_stack[operator_stack.len()];
+                    let o2 = operators.get(&operator_char).unwrap();
+
+                    if o1.precedence <= o2.precedence {
+                        let o2_char = operator_stack.pop().unwrap().to_string();
+                        output.push(o2_char);
+                    }
 
                 }
-                operator.push(&tokens[i])
             }
         }
     }
